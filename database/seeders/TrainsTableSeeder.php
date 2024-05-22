a337fc5841648c1f5a9f577aca5b5a38a085b553
simone-danzeri <s.danzeri@icloud.com>
2024-05-22 14:12:34 +0200

    feat: add faker newTrain instance

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Train;
+use Faker\Generator as Faker;
 
 class TrainsTableSeeder extends Seeder
 {
@@ -12,8 +14,20 @@ class TrainsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for($i = 0; $i < 20; $i++) {
+            $newTrain = new Train();
+            $newTrain->azienda = $faker->word();
+            $newTrain->stazione_di_partenza = $faker->city();
+            $newTrain->stazione_di_arrivo = $faker->city();
+            $newTrain->orario_di_partenza = $faker->time();
+            $newTrain->orario_di_arrivo = $faker->time();
+            $newTrain->codice_treno = $faker->numberBetween(1, 15000);
+            $newTrain->numero_carrozze = $faker->numberBetween(1, 10);
+            $newTrain->in_orario = $faker->numberBetween(0, 1);
+            $newTrain->cancellato = $faker->numberBetween(0, 1);
+            $newTrain->save();
+        }
     }
 }
