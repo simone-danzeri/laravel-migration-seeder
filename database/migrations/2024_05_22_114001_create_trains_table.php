56cc82948399a072aa1e3f1dd40d7760497d1557
simone-danzeri <s.danzeri@icloud.com>
2024-05-22 13:52:42 +0200

    feat: created Model

--- database/migrations/2024_05_22_114001_create_trains_table.php
+++ database/migrations/2024_05_22_114001_create_trains_table.php
@@ -15,6 +15,15 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('azienda', 75);
+            $table->string('stazione_di_partenza', 100);
+            $table->string('stazione_di_arrivo', 100);
+            $table->date('orario_di_partenza');
+            $table->date('orario_di_arrivo');
+            $table->unsignedSmallInteger('codice_treno');
+            $table->unsignedTinyInteger('numero_carrozze');
+            $table->unsignedTinyInteger('in_orario');
+            $table->unsignedTinyInteger('cancellato');
             $table->timestamps();
         });
     }
